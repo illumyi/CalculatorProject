3d9cef58dd3ac9aa152a1bfdd23476cce7f947a2
Temitayo Durojaiye <temi_durojaiye@outlook.com>
2026-04-11 20:16:16 +0100

    base program

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-print("hi")
+# Get inputs from user + operator validation
+operators = ["+", "-", "*", "/"]
+while True:
+    print("What operation would you like to perform?: ")
+    operation = input()
+    if operation in operators:
+        break
+    else:
+        print("Invalid operator, try again")
+        continue
+
+while True: # Number inputs validation 
+    try:
+        print("What is the first number?: ")
+        first = float(input())
+        break
+    except ValueError:
+        print("Invalid input, try again")
+
+while True:
+    try:
+        print("What is the second number?: ")
+        second = float(input())
+        break
+    except ValueError:
+        print("Invalid input, try again")
+
+# Calculation options
+def add(first, second):
+    return first + second
+def subtract(first, second):
+    return first - second
+def multiply(first, second):
+    return first * second
+def divide(first, second):
+    return first / second
+
+# Results
+operations = {"+": add,
+              "-": subtract,
+              "*": multiply,
+              "/": divide}
+
+print("Result = ", operations[operation](first, second))
